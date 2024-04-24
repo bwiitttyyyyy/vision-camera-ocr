9813c30e8adcfabbfc732c14239f1a6c13e2f116
Brandon Witt <witt@Brandons-MBP.lan>
2024-04-24 15:05:55 -0700

    chore: update import

--- ios/VisionCameraOcr.m
+++ ios/VisionCameraOcr.m
@@ -2,10 +2,4 @@
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/FrameProcessorPluginRegistry.h>
 
-#if __has_include("VisionCameraOcr/VisionCameraOcr-Swift.h")
-#import "VisionCameraOcr/VisionCameraOcr-Swift.h"
-#else
-#import "VisionCameraOcr-Swift.h"
-#endif
-
 VISION_EXPORT_SWIFT_FRAME_PROCESSOR(OCRFrameProcessorPlugin, scanOCR)
