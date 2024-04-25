02134c5c5e8ddc6db4ab3d045c4c371172bcd821
Brandon Witt <witt@Brandons-MBP.lan>
2024-04-25 13:48:31 -0700

    chore: use global imports

--- ios/VisionCameraOcr.m
+++ ios/VisionCameraOcr.m
@@ -1,6 +1,11 @@
+#if __has_include(<VisionCameraOcr/VisionCameraOcr-Swift.h>)
+
 #import <Foundation/Foundation.h>
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/FrameProcessorPluginRegistry.h>
 
-@interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(VisionCameraOcr, scanOCR)
-@end
+#import "VisionCameraOcr-Swift.h"
+
+VISION_EXPORT_SWIFT_FRAME_PROCESSOR(VisionCameraOcr, scanOCR)
+
+#endif
